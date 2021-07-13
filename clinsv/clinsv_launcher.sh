44ea0c9a7721e916d98997162bddd3009b0321f8
Mark Corbett <37854406+speleonut@users.noreply.github.com>
2021-07-13 17:12:41 +0930

    test clinSV

--- clinsv/clinsv_launcher.sh
+++ clinsv/clinsv_launcher.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
-#SBATCH -J prepGRIDSS
-#SBATCH -o /hpcfs/users/%u/log/prepGRIDSS-slurm-%j.out
+#SBATCH -J clinSV
+#SBATCH -o /hpcfs/users/%u/log/clinsv-slurm-%j.out
 
 #SBATCH -A robinson
 #SBATCH -p batch
 #SBATCH -N 1
-#SBATCH -n 10
-#SBATCH --time=1-00:00:00
-#SBATCH --mem=16GB
+#SBATCH -n 16
+#SBATCH --time=12:00:00
+#SBATCH --mem=48GB
 
 # Notification Configuration 
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=${USER}@adelaide.edu.au
 
-# A script to preprocess bam files for GRIDSS
-## List modules and file paths ##
+# This is the master script that coordinates job submission for the neurogenetics ClinSV structural variant pipeline.
+## Set hard-coded paths and define functions ##
 scriptDir="/hpcfs/groups/phoenix-hpc-neurogenetics/scripts/git/neurocompnerds/Structural-Variants"
-customModDir="/hpcfs/groups/phoenix-hpc-neurogenetics/executables/easybuild/modules/all"
-modList=("arch/skylake" "SAMtools/1.12" "Java/1.8.0_191" "BWA/0.7.17" "R/4.0.3")
-RLibDir="/hpcfs/groups/phoenix-hpc-neurogenetics/RefSeq/R/4.0.3/RLibs"
-threads=8
+logDir="/hpcfs/users/${USER}/log"
+modList=("arch/skylake" "Perl/5.28.0")
+
+if [ ! -d "${logDir}" ]; then
+    mkdir -p ${logDir}
+    echo "## INFO: New log directory created, you'll find all of the log information from this pipeline here: ${logDir}"
+fi
 
 usage()
 {
-echo "# This preprocesses BAM files for GRIDSS.  This script can be directly submitted to the scheduler but it is best to let gridss_launcher.sh handle this.
+echo "# This is the master script that coordinates job submission for analysis of one or many Illumina whole genomes using ClinSV.
 # The script will select the right parameters to work with either GRCh37/hg19 or GRCh38/hg38 genome builds.  
-# Requires: An aligned BAM file (or files), BWA, samtools, Java, R 
+# Requires: An aligned BAM file 
 #
-# Usage sbatch $0 -p file_prefix -i /path/to/input/bam-file [ -o /path/to/output -c /path/to/config.cfg ] | [ - h | --help ]
+# Usage $0 -p file_prefix -i /path/to/input/bam-file [ -o /path/to/output -c /path/to/config.cfg ] | [ - h | --help ]
 #
 # Options
 # -p	REQUIRED. A prefix to your sequence files of the form PREFIX*.bam
 # -i	REQUIRED. Path to the BAM file
 # -c	OPTIONAL. /path/to/config.cfg. A default config will be used if this is not specified.  The config contains all of the stuff that used to be set in the top part of our scripts
-# -o	OPTIONAL. Path to where you want to find your file output (if not specified an output directory /hpcfs/users/${USER}/GRIDSS/\${outPrefix} is used)
+# -o	OPTIONAL. Path to where you want to find your file output (if not specified an output directory /hpcfs/users/${USER}/clinsv/\${outPrefix} is used)
 # -h or --help	Prints this message.  Or if you got one of the options above wrong you'll be reading this too!
 # 
 # Original: Mark Corbett, 06/07/2021 
@@ -70,7 +73,7 @@ done
 
 ## Pre-flight checks ##
 if [ -z "$Config" ]; then # If no config file specified use the default
-    Config=$scriptDir/configs/hs38DH.SV_GRIDSS.phoenix.cfg
+    Config=$scriptDir/configs/hs38DH.SV_ClinSV.phoenix.cfg
     echo "## INFO: Using the default config ${Config}"
 fi
 source $Config
@@ -91,29 +94,21 @@ if [ ! -f "$bamFile" ]; then
     exit 1
 fi 
 if [ -z "$workDir" ]; then # If no output directory then set and create a default directory
-	workDir=/hpcfs/users/${USER}/GRIDSS/$outPrefix
+	workDir=/hpcfs/users/${USER}/clinsv/$outPrefix
 	echo "## INFO: Using $workDir as the output directory"
 fi
 if [ ! -d "$workDir" ]; then
-    mkdir -p $workDir
-fi
-tmpDir=$baseTmpDir/$outPrefix
-if [ ! -d "$tmpDir" ]; then
-    mkdir -p $tmpDir
+	mkdir -p $workDir
 fi
 
 ## Load modules ##
-module use $customModDir
 for mod in "${modList[@]}"; do
     module load $mod
 done
-export R_LIBS_USER=${RLibDir}
 
-## Have at thee villain ##
-${gridss_cmd_common} \
--t ${threads} \
--s preprocess \
--o $workDir/$outPrefix.sv.vcf.gz \
--a $workDir/$outPrefix.asm.bam \
--w $tmpDir \
-${bamFile} > $workDir/$outPrefix.gridss.log 2>&1
+## Launch ClinSV ##
+$clinsv_cmd_common \
+-p $workDir \
+-r all \
+-i $inputDir \
+-f 
